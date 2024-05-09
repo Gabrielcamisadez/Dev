d3c1518a11a6e2f0c2fc6168de62fa85b83e5a47
Gabrielcamisadez <129340371+Gabrielcamisadez@users.noreply.github.com>
2024-05-08 21:30:26 -0300

    Create conexao.php
    conexao com PDO

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$user = 'hoot';
+$password = '';
+$bd = "banco5";
+
+conexao mysqli_connect($host, $user, $password, $bd);
+
+$name = "";
+$idade = "";
+$email = "";
+$bd = "lojaXYZ";
+    mysqli_connect($name, $idade, $email)
+
