7b6625c4ba8aff9db2edd21032735c9fa01edc9e
Gabrielcamisadez <129340371+Gabrielcamisadez@users.noreply.github.com>
2024-05-22 21:08:55 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -12,3 +12,17 @@ $email = "";
 $bd = "lojaXYZ";
     mysqli_connect($name, $idade, $email)
 
+        ?>
+
+        <?php
+
+try{
+    global $conn;
+    $conn = new PDO("mysql:dbname=banco5es;host=localhost;","root", "12345");
+}catch(PDException $e){
+    echo "Erro no PDO". $e->getMessage();
+}catch(Exception $e){
+    echo "Erro no Generico ". $e -> getMessage();
+}
+    
+
